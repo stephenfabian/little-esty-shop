bd665e4a3eac33f1d9656491a283fbf87be265d7
Stephen Fabian <fabianstephen@gmail.com>
2022-09-24 22:47:45 -0600

    adds User Story 8 - Admin Invoice Show Page: Total Revenue and Discounted Revenue

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin invoices show page' do
-  before :each do
-    names_array = {'gjcarew' => 22, 'stephenfabian' => 25, 'Rileybmcc' => 22, 'KevinT001' => 11}
-    allow(GithubFacade).to receive(:commits).and_return(names_array)
+  # before :each do
+  #   names_array = {'gjcarew' => 22, 'stephenfabian' => 25, 'Rileybmcc' => 22, 'KevinT001' => 11}
+  #   allow(GithubFacade).to receive(:commits).and_return(names_array)
 
-    pull_requests_count = 3
-    allow(GithubFacade).to receive(:pull_requests).and_return(pull_requests_count)
-  end
+  #   pull_requests_count = 3
+  #   allow(GithubFacade).to receive(:pull_requests).and_return(pull_requests_count)
+  # end
   describe 'When I visit an admin invoice show page' do
     before :each do 
       @customer = Customer.create!(first_name: 'Jeff', last_name: 'Bridges')
@@ -116,6 +116,44 @@ RSpec.describe 'Admin invoices show page' do
       click_button 'Update Invoice Status'
       expect(current_path).to eq(admin_invoice_path(invoice.id))
       expect(invoice.reload.status).to eq('completed')
+    end
+  end
+
+  describe 'Admin Invoice Show Page: Total Revenue and Discounted Revenue' do
+    describe 'As admin, when I visit an admin invoice show page' do
+      describe 'I see the total revenue from this invoice (not including discounts)' do
+        it 'And I see the total discounted revenue from this invoice which includes bulk discounts in the calculation' do
+
+          merchant_stephen = Merchant.create!(name: "Stephen's Shady Store")
+          merchant_roger = Merchant.create!(name: "Roger's Fancy Store")
+          customer1 = Customer.create!(first_name: "Abdul", last_name: "Redd")
+
+          item_toothpaste = merchant_stephen.items.create!(name: "Item Toothpaste", description: "The worst toothpaste you can find", unit_price: 4000 )
+          item_lamp = merchant_stephen.items.create!(name: "Item Lamp", description: "You bet, it's a lamp", unit_price: 250)
+          item_desk = merchant_roger.items.create!(name: "Item Desk", description: "sturdy desk", unit_price: 250)
+
+          invoice1 = customer1.invoices.create!(status: "completed")
+          invoice2 = customer1.invoices.create!(status: "completed")
+          invoice3 = customer1.invoices.create!(status: "completed")
+
+          invoice_itemA = invoice1.invoice_items.create!(quantity: 100, unit_price: 2500, status: "packaged", item_id: item_toothpaste.id)
+          invoice_itemB = invoice1.invoice_items.create!(quantity: 15, unit_price: 1500, status: "packaged", item_id: item_lamp.id)
+          invoice_itemC = invoice1.invoice_items.create!(quantity: 15, unit_price: 1500, status: "packaged", item_id: item_desk.id)
+
+          bulk_discountA = merchant_stephen.bulk_discounts.create(percentage_discount: 20, quantity_threshold: 10)
+          bulk_discountB = merchant_stephen.bulk_discounts.create(percentage_discount: 15, quantity_threshold: 15)
+          bulk_discountC = merchant_stephen.bulk_discounts.create(percentage_discount: 50, quantity_threshold: 200)
+          
+          bulk_discountD = merchant_roger.bulk_discounts.create(percentage_discount: 18, quantity_threshold: 20)
+          bulk_discountE = merchant_roger.bulk_discounts.create(percentage_discount: 25, quantity_threshold: 15)
+
+          visit admin_invoice_path(invoice1.id) 
+
+          expect(page).to have_content("Total Revenue: $2,950.00")
+          expect(page).to have_content("Total Discounted Revenue: $2,348.75")
+
+        end
+      end
     end
   end
 end
